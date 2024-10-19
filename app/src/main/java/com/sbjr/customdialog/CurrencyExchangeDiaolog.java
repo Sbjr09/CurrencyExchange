7b0c5a5e20165080db79bc1593e2db4b9aa69190
Sanket <sanketbh1598@gmail.com>
2024-10-19 13:02:44 +0530

    com

---
+++ app/src/main/java/com/sbjr/customdialog/CurrencyExchangeDiaolog.java
@@ -0,0 +1,61 @@
+//package com.sbjr.customdialog;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.jetbrains.annotations.NotNull;
+
+//public class CurrencyExchangeDiaolog extends Dialog {
+//    RadioGroup radioGroup;
+//    TextView textViewConverted;
+//
+//
+//    interface OncurrencyExchangeLIstner{
+//        public void onCurrencyExchange();
+//    }
+//
+//    public OncurrencyExchangeLIstner oncurrencyExchangeLIstner;
+//
+//    public CurrencyExchangeDiaolog(@NotNull Context context){
+//        super(context);
+//        setContentView(R.layout.currency_exchange_dialog);
+//        initViews();
+//        initListeners();
+//
+//    }
+//
+//    public void initViews(){
+//        radioGroup = findViewById(R.id.radioGroupCurrencies);
+//        textViewConverted = findViewById(R.id.textViewConvertedAmount);
+//
+//    }
+//
+//    public void initListeners(){
+//        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+//                RadioButton radioButton = radioGroup.findViewById(i);
+//
+//
+//
+//            }
+//        });
+//    }
+
+
+
+
+
+
+
+
+
